391bb4ad16bc2862ca46ef48df86c3e91d9d93d2
SCHOENFELDER FRANCK <franck.schoenfelder@etu.unistra.fr>
2026-01-22 18:48:39 +0100

    ui(vault) Arranged the ui of the filtering chips

--- app/src/main/java/fr/ailurus/housepassp2p/ui/components/vault/FilterChipsRow.kt
+++ app/src/main/java/fr/ailurus/housepassp2p/ui/components/vault/FilterChipsRow.kt
@@ -1,10 +1,16 @@
 package fr.ailurus.housepassp2p.ui.components.vault
 
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import fr.ailurus.housepassp2p.AppDimensions
 import fr.ailurus.housepassp2p.data.entities.GroupSummary
 import fr.ailurus.housepassp2p.ui.theme.HousePassP2PTheme
 
@@ -14,9 +20,17 @@ fun FilterChipsRow(
     selectedGroup: GroupSummary?,
     onGroupSelected: (GroupSummary) -> Unit
 ) {
-    Row {
+    val scrollState = rememberScrollState()
+
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = AppDimensions.PaddingMedium)
+            .horizontalScroll(scrollState)
+    ){
         groups.forEach { group ->
             FilterChip(
+                modifier = Modifier.padding(horizontal = AppDimensions.PaddingSmall),
                 selected = group == selectedGroup,
                 onClick = { onGroupSelected(group) },
                 label = { Text(group.name) }
